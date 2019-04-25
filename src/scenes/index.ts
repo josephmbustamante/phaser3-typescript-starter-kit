022544b906c2e2d912e7e74c7e3966d30c74742e
Joe Bustamante <joe.bustamante@atomicobject.com>
2019-04-25 16:01:42 -0400

    Remove deleted scenes from scene list

--- src/scenes/index.ts
+++ src/scenes/index.ts
@@ -1,17 +1,7 @@
 import { MainMenuScene } from './main-menu-scene';
-import { UIScene } from './ui-scene';
-import { GameScene } from './game-scene';
 import { BootScene } from './boot-scene';
 
-/**
- * The order of this list matters. Scenes are drawn and evaluated in reverse order.
- * This also means that events are handled in drawing order - if you have multiple active scenes,
- * an input event will go to the last scene in the draw list that is being drawn where that event occurred.
- * Because of that, UI and Menu scenes that exist alongside the game should be drawn after the game scenes.
- */
 export default [
   BootScene,
   MainMenuScene,
-  GameScene,
-  UIScene,
 ];
